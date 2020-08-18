159dacb92c7883435b4563ac15eed9ce17d39fc3
Chinseok Lee <me@askcompany.kr>
2020-08-18 20:18:08 +0900

    initial commit

---
+++ dj_http_trigger/__init__.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(0, 'dj')
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj.settings")
+
+import azure.functions as func
+from dj.wsgi import application
+
+wsgi = func.WsgiMiddleware(app=application)
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return wsgi.main(req, context)
